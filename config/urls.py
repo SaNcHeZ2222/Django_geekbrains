85c75051c4c3f3084a70128038f075169f86eef5
SaNcHeZ2222 <sibaybakov@gmail.com>
2023-01-26 14:22:18 +0400

    lesson_4 Display model's data

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
-from django.contrib import admin
+from django. contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
+from django. views.generic import RedirectView
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path ("admin/", admin.site.urls),
     path("", RedirectView.as_view(url="mainapp/")),
-    path("mainapp/", include("mainapp.urls", namespace="mainapp")),
+    path( "mainapp/", include ("mainapp.urls", namespace="mainapp")),
 ]
